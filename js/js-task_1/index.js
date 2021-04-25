f4eafccfc9ebd9d65cd2e199103bde94e6c8ad27
OlgaIgnat <82769866+OlgaIgnat@users.noreply.github.com>
2021-04-25 19:18:13 +0300

    js1

---
+++ js/js-task_1/index.js
@@ -0,0 +1,99 @@
+let str="Привет",
+    sum=123,
+    num=15.8,
+    flag=true,
+    txt="true";
+console.log(str,typeof(str));
+console.log(str,typeof(str));
+console.log(sum,typeof(sum));
+console.log(num,typeof(num));
+console.log(flag,typeof(flag));
+console.log(txt,typeof(txt));
+let a1= 5 % 3;
+    a2= 3 % 5;
+    a3= 5 + '3';
+    a4='5'-3;
+    a5=75 + 'кг';
+    a6=785 * 653;
+    a7= 100 / 25;
+    a8= 0 * 0;
+    a9= 0 / 2;
+    a10=89 / 0;
+    a11= 98 + 2;
+    a12=5-98;
+    a13=(8 + 56 * 4) / 5;
+    a14=(9 - 12) * 7 / (5 + 2);
+    a15= +"123";
+    a16=1 || 0;
+    a17=false || true;
+    a18= true > 0;
+console.log(a1,typeof(a1));
+console.log(a2,typeof(a2));
+console.log(a3,typeof(a3));
+console.log(a4,typeof(a4));
+console.log(a5,typeof(a5));
+console.log(a6,typeof(a6));
+console.log(a7,typeof(a7));
+console.log(a8,typeof(a8));
+console.log(a9,typeof(a9));
+console.log(a10,typeof(a10));
+console.log(a11,typeof(a11));
+console.log(a12,typeof(a12));
+console.log(a13,typeof(a13));
+console.log(a14,typeof(a14));
+console.log(a15,typeof(a15));
+console.log(a16,typeof(a16));
+console.log(a17,typeof(a17));
+console.log(a18,typeof(a18));
+
+let v=10;
+let VCilindra=v*a7;
+console.log(VCilindra);
+
+const p=3.14;
+let r=5;
+let SKruga=p*(r**2);
+console.log(SKruga);
+
+let a=5;
+    b=7;
+    h=10;
+let STrap=(a+b) /2*h;
+console.log(STrap);
+
+a=8;
+b=3;
+let x=(16-a)/2+b;
+console.log(x);
+
+x=(a-15*b)/(b-6);
+console.log(x);
+
+x=23780/(3+a+b);
+console.log(x);
+
+let t1='индо земля зашаталась под ногами-и вырос,';
+    t2='и заревел он голосом диким...';
+    t3='блеснула молния и ударил гром,';
+    t4= 'а так какое-то чудище, страшное и мохнатое,';
+    t5= 'как будто из-под земли, перед купцом:';
+    t6='Он подошёл и сорвал аленький цветочек.';
+    t7='зверь не зверь, человек не человек,';
+    t8='В ту же минуту, безо всяких туч,';
+let  text=t6+t8+t3+t1+t5+t7+t4+t2;
+console.log(text);
+ 
+let nameUser=prompt('What is your name?');
+console.log(nameUser);
+console.log("Hello,"+nameUser+" how are you doing today?");
+
+
+
+
+
+
+
+
+
+
+
